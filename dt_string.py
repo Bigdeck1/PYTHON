379e38ae6d0a9ea3f4d39b081760e27d9f8a6bbe
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-11-24 02:47:53 +0800

    Add files via upload
    string datatype

---
+++ dt_string.py
@@ -0,0 +1,7 @@
+#string data type
+first_Name = "Barakuda"
+last_Name = " the greet "
+full_Name = first_Name + last_Name
+print ("hello" + last_Name +""+ first_Name)
+print ("hello " + full_Name)
+print(type(first_Name))
