bb8e9c4a2094274c7c14f8be8920b381c0288cf3
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-11-26 20:57:24 +0800

    Add files via upload

---
+++ string manip.py
@@ -0,0 +1,13 @@
+#strings modification
+name = "Ivan"
+print(len( name))
+print (name.find("I"))
+print (name.find("n"))
+print (name.capitalize())
+print (name.upper())
+print (name.lower())
+print (name.isdigit())
+print (name.isalpha())
+print (name.count("a"))
+print (name.replace("n","a"))
+print (name*3)
---
+++ typecasting.py
@@ -0,0 +1,16 @@
+#type casting
+i = 6 #int
+v = 6.9 #float
+a = "69.69" #string
+
+i = float(i)
+v = float(v)
+a = float(a)
+
+print("i is :"+str(i))
+print("v is: "+str(v))
+print("a is :"+str(a))
+
+print (i)
+print (v)
+print (a)
