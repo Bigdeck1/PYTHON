8e18ec5c3e528ed3edc5e1b78f5f7ff6106267b1
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-11-26 21:31:46 +0800

    Add files via upload

---
+++ inputfunc.py
@@ -0,0 +1,11 @@
+#input function
+
+name = input("Whats your name?: ")
+age = int(input("How old are you?: "))
+#age = input("How old are you?: ")
+#age = int(age)
+age = age+1
+
+print (f"Hello{name}!")
+print ("Happy Bornday")
+print (f"Your ages now is {age}!")
