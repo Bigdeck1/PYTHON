e80677c2649fdc6da1168533f27e1d7985dd7d1a
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-11-29 18:26:21 +0800

    Add files via upload

---
+++ mathvar.py
@@ -0,0 +1,20 @@
+#augmented arithmethic
+
+bodycount = 15
+
+#bodycount = bodycount + 1
+#bodycount += 1
+#bodycount = bodycount - 1
+#bodycount -= 1
+#bodycount = bodycount * 4
+#bodycount *= 4
+#bodycount = bodycount / 2
+#bodycount /= 4
+#bodycount = bodycount **2
+bodycount **= 2
+#bodycount = bodycount % 4
+#bodycount %= 4
+
+
+
+print(bodycount)
