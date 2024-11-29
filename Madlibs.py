85e8a9af1e5daed79f41184a95cfd697c9671a9a
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-11-29 18:09:21 +0800

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,12 @@
+#Madlibs
+who1 = input("Saan mo gusto tumambay?: ")
+sino1 = input("Sino crush mo?: ")
+sino2 = input("Sino kaaway mo?: ")
+ano1 = input ("Ano huli mo kinain?: ")
+ano2 = input ("Ano huli mo ginawa?: ")
+nangyari1 = input("Ano favorite mo sports?: ")
+
+print(f"Ako ay nasa {who1}")
+print(f"nakita ko si {sino1} kayakap si {sino2}")
+print(f"dahil dun ako ay {ano1} sa akin pagkaka {ano2}")
+print(f"kaya ako ay {nangyari1} nalang")
