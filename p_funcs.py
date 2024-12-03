74c5142f3b354bd183b6b2915940160f19e7407e
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-12-03 09:50:08 +0800

    Add files via upload

---
+++ p_funcs.py
@@ -0,0 +1,14 @@
+import math
+
+pi = 3.1416
+x = 6
+y = 9
+z =3
+#print(round(pi))
+#print(math.ceil(pi))
+#print(math.floor(pi))
+#rint(abs(pi))
+#print(pow(pi,2))
+#print(math.sqrt(pi420))
+#print(max(x,y,z))
+print(min(x,y,z))
