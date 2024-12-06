a3d24868ce004e0d38d4f082ded6aabac3d28926
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-12-06 21:08:28 +0800

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,12 @@
+#if statement
+
+age = int(input("How old are you?"))
+
+if age == 100:
+    print("Maris racal")
+elif age >= 18:
+    print("You are old enough")
+elif age< 0:
+    print("how can you use computer?")
+else:
+    print("DAMNNN DIDDY")
