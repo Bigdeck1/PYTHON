8707539fc380ff579d1b0e5ecd9f4a9ad90914a2
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-12-06 21:17:32 +0800

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,13 @@
+#logical operators
+
+goods = int(input("Kamusta ka?"))
+
+if goods <=0 or goods >=30:
+    print("nice mah g goods lang din naman ako")
+elif goods >20 and goods <30:
+    print("tuss kana man negative na tayo dito mah g")
+else:
+    print("awit sayo man bat ganyan trip mo")
+
+#if not(goods <=15 or goods >=19):
+    print("nice mah g goods lang din naman akoww")
