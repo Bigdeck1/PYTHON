69553d1b5cf94aa155438a64f33db10fc9a4428a
Clarence Ivan R. Riveral Pedrano <158130506+Bigdeck1@users.noreply.github.com>
2024-12-11 14:36:37 +0800

    Add files via upload

---
+++ i want to die!!!!!!!.py
@@ -0,0 +1,11 @@
+while True:
+    print("die or Die")
+    print("a for die, b for Die")
+    want = input("What you want ?: ")
+    if want == "a":
+        print("Soon you'll meet satan or God")
+    elif want == "b":
+        print("I badly wanna die for this sake life bruh")
+    else:
+        print("there's no other option i wanna die i dont wanna live like this anymore!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    break
